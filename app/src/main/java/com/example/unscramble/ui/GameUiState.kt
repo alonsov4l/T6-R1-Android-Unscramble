78e781ad5c3cffc95548cf1263b085c27bc2247b
Andre <andre.valenzuela@unmsm.edu.pe>
2026-09-20 18:20:54 -0500

    Ruta1-Unscramble

---
+++ app/src/main/java/com/example/unscramble/ui/GameUiState.kt
@@ -0,0 +1,13 @@
+package com.example.unscramble.ui
+
+import kotlinx.coroutines.flow.MutableStateFlow
+
+private val _uiState = MutableStateFlow(GameUiState())
+
+data class GameUiState(
+    val currentScrambledWord: String = "",
+    val currentWordCount: Int = 1,
+    val isGuessedWordWrong: Boolean = false,
+    val score: Int = 0,
+    val isGameOver: Boolean = false,
+)
